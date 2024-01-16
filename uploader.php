0dac5919b6084ea354519364303e14ea6d63778a
bil022 <bil022@ucsd.edu>
2024-01-15 22:57:21 -0800

    uploader

--- uploader.php
+++ uploader.php
@@ -21,34 +21,30 @@
       $input = $_REQUEST;
     }
 
-    print_r($input);
-
     $upload="NA";
     foreach ($input->json as $item) {
       if ($item->{'name'}=="thumb")
         $upload=$item->{'value'};
     }
-    $input->status="OK";
+    $status="OK";
     if ($upload!=$thumb) {
       $headers = @get_headers($thumb, 1); // @ to suppress errors. Remove when debugging.
       if (isset($headers['Content-Type'])) {
         if (strpos($headers['Content-Type'], 'image/png') === FALSE) {
-          $input->status="Not a png image: ".$thumb;
+          $status="Not a png image: ".$thumb;
         } else {
           // copy image & update thumb
           $content = file_get_contents($upload);
           $ret=file_put_contents('uploader/uploader.png', $content);
-          if ($ret) {
-            $input->json["thumb"]=$thumb;
-          } else {
-            $input->status="Cannot save file: ".$upload;
+          if (!$ret) {
+            $status="Cannot save file: ".$upload;
           }
         }
       }
     } else {
-      $input->status="Unknown link: ".$thumb;
+      $status="Unknown link: ".$thumb;
     }
   }
-  print json_encode($input, JSON_PRETTY_PRINT);
+  print json_encode('{"status":"'.$status.'"}', JSON_PRETTY_PRINT);
 ?>
 
